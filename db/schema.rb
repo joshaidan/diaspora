fd526717462a041fb7f6950076a98a9087312184
zhitomirskiyi <ilya@joindiaspora.com>
2011-04-27 12:31:07 -0700

    WIP, need to figure out stubbing in job spec, moved receive tokens to the contact, need to special case getting an access token with a refesh token.

--- db/schema.rb
+++ db/schema.rb
@@ -225,6 +225,7 @@ ActiveRecord::Schema.define(:version => 20110412230914) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "mongo_id"
+    t.datetime "fetched_at"
   end
 
   add_index "people", ["diaspora_handle"], :name => "index_people_on_diaspora_handle", :unique => true
